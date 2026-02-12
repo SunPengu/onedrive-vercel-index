e80168b591194f54b80966d17961e05638d19ae9
Lin DanPeng <163846561+SunPengu@users.noreply.github.com>
2026-02-12 22:27:01 +0800

    Remove 'public/' prefix from protectedRoutes

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/public/小秘密，不准偷看哟🤪'],
+  protectedRoutes: ['/小秘密，不准偷看哟🤪'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:pengg@linux.do',
